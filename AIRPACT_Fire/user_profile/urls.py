f84cd7cf2869c15be182f54756edb865f989dbdf
jbaird <justin.baird@wsue.edu>
2016-03-23 23:52:19 -0700

    added proper url pattern for appauth

--- AIRPACT_Fire/user_profile/urls.py
+++ AIRPACT_Fire/user_profile/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^register$', views.register_user),
     url(r'^loggedin$', views.loggedin),
     url(r'^invalid$', views.invalid_login),
+    url(r'^appauth$',views.user_app_auth),
     url(r'^register_success$', views.register_success),
     #include('file_upload.urls')
     #more to come...
