6264d1cc5e2c2a393d5f74eed9487b84a5405e87
jbaird <justin.baird@wsue.edu>
2016-05-08 20:53:12 -0700

    .

--- AIRPACT_Fire/AIRPACT_Fire/wsgi.py
+++ AIRPACT_Fire/AIRPACT_Fire/wsgi.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AIRPACT_Fire.settings")
-application = django.core.handlers.wsgi.WSGIHandler()
 
-# from django.core.wsgi import get_wsgi_application
-
-
-
-# application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
