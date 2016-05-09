5ae0efb53d1d188f563e561d64886225eeb7a32d
jbaird <justin.baird@wsue.edu>
2016-05-08 20:38:57 -0700

    .

--- AIRPACT_Fire/AIRPACT_Fire/wsgi.py
+++ AIRPACT_Fire/AIRPACT_Fire/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AIRPACT_Fire.settings")
+# from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AIRPACT_Fire.settings")
+
+# application = get_wsgi_application()
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py run_gunicorn
+web: web: gunicorn AIRPACT_Fire.wsgi.application
