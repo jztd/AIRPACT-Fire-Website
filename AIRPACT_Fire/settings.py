f8b831681e69a6acb0701e04d39ddc430f57e36b
jbaird <justin.baird@wsue.edu>
2016-05-12 23:24:03 -0700

    .

--- AIRPACT_Fire/settings.py
+++ AIRPACT_Fire/settings.py
@@ -166,5 +166,5 @@ if not DEBUG:
    AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-   S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+   S3_URL = 'http://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
    STATIC_URL = S3_URL
