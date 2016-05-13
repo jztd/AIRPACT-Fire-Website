fe33d8d1196e495c4531c1ceacfe95b2f6741c39
jbaird <justin.baird@wsue.edu>
2016-05-12 19:05:22 -0700

    .

--- AIRPACT_Fire/settings.py
+++ AIRPACT_Fire/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -159,3 +159,4 @@ LOGIN_REDIRECT_URL='/user/'
 LOGIN_URL='/user/'
 TIME_ZONE = "America/Los_Angeles"
 USE_TZ = True
+DATABASES['default'] = dj_database_url.config()
