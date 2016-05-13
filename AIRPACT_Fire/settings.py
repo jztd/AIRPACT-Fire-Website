61f42f12a6a03cd71f23ac510ff50774fbd3eb55
jbaird <justin.baird@wsue.edu>
2016-05-12 23:48:35 -0700

    .

--- AIRPACT_Fire/settings.py
+++ AIRPACT_Fire/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     #'threadedcomments',
     'django.contrib.sites',
     'django_comments',
-    'gunicorn'
+    'gunicorn',
+    'storages'
     #'fluent_comments',
     #'crispy_forms',
 ]
@@ -162,11 +163,24 @@ USE_TZ = True
 DATABASES['default'] = dj_database_url.config()
 
 
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+AWS_STORAGE_BUCKET_NAME = 'airpactfire'
+AWS_ACCESS_KEY_ID = 'AKIAJS7IVSXCUDE4GWVQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+    # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+    # We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+    # refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+    # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+    # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
+
 
 # STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
