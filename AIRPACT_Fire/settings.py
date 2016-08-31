c05f7c74d43c822e412ce6cf8c29994ed3d24918
jbaird <justin.baird@wsue.edu>
2016-08-30 23:35:06 -0700

    added option for toggling development and production for image stoarges

--- AIRPACT_Fire/settings.py
+++ AIRPACT_Fire/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
+PRODUCTION = 1
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,26 +164,31 @@ USE_TZ = True
 DATABASES['default'] = dj_database_url.config()
 
 
-
-AWS_STORAGE_BUCKET_NAME = 'airpactfire'
-AWS_ACCESS_KEY_ID = 'AKIAJS7IVSXCUDE4GWVQ'
-AWS_SECRET_ACCESS_KEY = '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'
+if PRODUCTION is 1:
+    AWS_STORAGE_BUCKET_NAME = 'airpactfire'
+    AWS_ACCESS_KEY_ID = 'AKIAJS7IVSXCUDE4GWVQ'
+    AWS_SECRET_ACCESS_KEY = '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'
 
     # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
     # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
     # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
     # We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
     # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
     # refers directly to STATIC_URL. So it's safest to always set it.
-STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+    STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
 
     # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
     # you run `collectstatic`).
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
-# STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
-MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    # STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+    MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static/'), ]
