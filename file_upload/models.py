6f14b2c5df6de8751c0c45ed5902fe2b93f2c35c
jbaird <justin.baird@wsue.edu>
2016-09-26 23:02:44 -0700

    added drawing of the circles to the images and saving them as copies

--- file_upload/models.py
+++ file_upload/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from user_profile.models import AirpactUser
 from django.db import models
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw
 from cStringIO import StringIO
 from django.core.files.uploadedfile import SimpleUploadedFile
 import os
@@ -9,6 +9,7 @@ import os
 class picture(models.Model):
 	pic = models.ImageField(upload_to = 'static/pictures/')
 	thumbnail = models.ImageField(upload_to = 'static/thumbnails/', null=True, blank=True)
+	pictureWithCircles = models.ImageField(upload_to = 'static/circles/', null=True, blank=True)
 	uploaded = models.DateTimeField(auto_now_add = True)
 	description = models.TextField(default = "")
 	user = models.ForeignKey(AirpactUser, on_delete=models.CASCADE)
@@ -17,6 +18,8 @@ class picture(models.Model):
 	highX = models.FloatField(null=False, default=0)
 	highY= models.FloatField(null=False, default=0)
 	lowColor = models.IntegerField(null=False, default=0)
+	lowX = models.FloatField(null=False, default=0)
+	lowY = models.FloatField(null=False,default=0)
 	geoX = models.FloatField(default = 46.7298)
 	geoY = models.FloatField(default =  -117.181738)
 
@@ -25,7 +28,6 @@ class picture(models.Model):
 		imageType = self.pic.file.content_type
 
 		#see what kind of file we are dealing with 
-		print(imageType)
 		if imageType == "image/jpeg":
 			pilImageType = "jpeg"
 			fileExtension = "jpg"
@@ -48,18 +50,57 @@ class picture(models.Model):
 		self.thumbnail.save('%s.%s'%(os.path.splitext(suf.name)[0],fileExtension), suf, save=False)
 
 
+	# creates a copy of the image with the circle points drawn on them
+	def generateCircles(self):
+		imageType = self.pic.file.content_type
 
+		#see what kind of file we are dealing with 
+		if imageType == "image/jpeg":
+			pilImageType = "jpeg"
+			fileExtension = "jpg"
+		elif imageType == "image/png":
+			pilImageType = "png"
+			fileExtension = "png"
 
+		#get the bounding boxes for each of the circles
+		highCords = [(self.highY-100, self.highX-100),(self.highY+100, self.highX+100)]
+		lowCords = [(self.lowY-100, self.lowX-100),(self.lowY+100, self.lowX+100)]
+
+		#open original image
+		OriginalImage = Image.open(StringIO(self.pic.read()))
+
+		#open a new drawing object with our image
+		editor = ImageDraw.Draw(OriginalImage)
+
+		#draw the cricles
+		editor.eclipse(highCords, outline=0)
+		editor.eclipse(lowCords, outline=0)
+
+		#get rid of the drawer
+		del editor
+
+		#create a place to hold the new image for a second
+		tempHandle = StringIO()
+
+		#write the image to the handler
+		OriginalImage.save(tempHandle, pilImageType)
+		tempHandle.seek(0)
+
+		#save the image
+		suf = SimpleUploadedFile(os.path.split(self.pic.name)[-1],tempHandle.read(),content_type=imageType)
+		self.pictureWithCircles.save('%s.%s'%(os.path.splitext(suf.name)[0],fileExtension), suf, save=False)
 
 
 	def save(self):
 
 		self.generateThumbnail()
+		self.generateCircles()
 		super(picture,self).save()
+	
 	def __str__(self):
 		return self.description
 
-# THE BITCHIN TIGGLE TAG
+# We called them tags, they are really the string representation of where a picture was taken uploaded by the user
 class tag(models.Model):
 	picture = models.ForeignKey(picture, on_delete=models.CASCADE)
 	text = models.TextField(null=False)
